9d5be9ef97eff6e786a358b0680dd50cf004a5ed
Ajey Simha <ajey.simha@sakhatech.com>
2016-11-02 11:51:26 +0530

    First commit

--- LocationManager/ViewController.swift
+++ LocationManager/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.getLocation()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +22,67 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func getLocation(){
+        print("my location is - ")
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        switch status {
+        case CLAuthorizationStatus.restricted:
+            print("restricted")
+            break
+        case CLAuthorizationStatus.denied:
+            print("denied")
+            break
+        case CLAuthorizationStatus.notDetermined:
+            print("not determined")
+            break
+        case CLAuthorizationStatus.authorizedWhenInUse:
+            print("allowed")
+            break
+        case CLAuthorizationStatus.authorizedAlways:
+            print("allowed")
+            break
+        default:
+            print("allowed")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error while updating location " + error.localizedDescription)
+    }
+    
+    //func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks, error)->Void in
+            if (error != nil) {
+                print("Reverse geocoder failed with error" + error!.localizedDescription)
+                self.locationManager.stopUpdatingLocation()
+                return
+            }
+            
+            if placemarks!.count > 0 {
+                let pm = placemarks![0]
+                print(pm.country)
+                print(pm.administrativeArea)
+                print(pm.locality)
+                print(pm.postalCode)
+                print(pm.isoCountryCode)
+                print(pm.location?.coordinate.latitude)
+                print(pm.location?.coordinate.longitude)
 
-
+                self.locationManager.stopUpdatingLocation()
+            }
+            else {
+                print("Problem with the data received from geocoder")
+            }
+        })
+    }
 }
 
